3058c8ce1b88b902c33e83637e2d5fc5d50237d3
Your Name <Razi1379>
2022-08-24 23:13:46 +0430

    HomeWork12

---
+++ Database.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO Karmand VALUES (0001, 'ali', 'hasani','9855498465', '34', 'mard','25000');
+INSERT INTO Karmand VALUES (0002, 'hasan', 'qavidel','98565498462', '0072', 'mard','75000');
+INSERT INTO Karmand VALUES (0003, 'raha', 'rameshi','9855497867', '2324', 'khanom','28000');
+INSERT INTO Karmand VALUES (0004, 'shayan', 'babayi','9853698485', '8883', 'mard','35000');
+INSERT INTO Karmand VALUES (0005, 'ramin', 'sabzevari','9855498225', '74568', 'mard','96000');
+INSERT INTO Karmand VALUES (0006, 'mahshad', 'miri','9851678466', '1238', 'mard','25000');
+INSERT INTO Karmand VALUES (0007, 'sharare', 'sanayi','9856723460', '7634', 'khanom','215000');
+
+
+INSERT INTO Vahed_Edari VALUES (0001, 'esfahan','3445', 'frosh','25000');
+INSERT INTO Vahed_Edari VALUES (0002, 'tehran', '0072', 'frosh','75000');
+INSERT INTO Vahed_Edari VALUES (0003, 'rasht', '2324', 'frosh','28000');
+INSERT INTO Vahed_Edari VALUES (0004, 'qazvin', 'frosh','35000');
+INSERT INTO Vahed_Edari VALUES (0005, 'qom','74568', 'frosh','96000');
+INSERT INTO Vahed_Edari VALUES (0006, 'kashan''1238', 'frosh','25000');
+INSERT INTO Vahed_Edari VALUES (0007, 'ilam''7634', 'kharid','215000');
+
+INSERT INTO Shobe VALUES (0001, 'enqelab','1379','tehran');
+INSERT INTO Shobe VALUES (0002, 'dr hesabi','1378', 'ilam');
+INSERT INTO Shobe VALUES (0003, 'rajayi','1365','qazvin');
+INSERT INTO Shobe VALUES (0004, 'SHAYANI','1389','kashan');
+INSERT INTO Shobe VALUES (0005, 'aLIPOR','1386','rasht');
+INSERT INTO Shobe VALUES (0006, 'HAFEZ','1390', 'qom');
+INSERT INTO Shobe VALUES (0007, 'CHAMRAN','1400', 'esfahan');
